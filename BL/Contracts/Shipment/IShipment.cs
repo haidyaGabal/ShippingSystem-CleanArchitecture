672473a2c1303f35b1a58e090bd8526015d10685
Haidy Gabal <haidy.a.gabal2024@gmail.com>
2026-04-08 21:42:30 +0200

    add shipment in BL

---
+++ BL/Contracts/Shipment/IShipment.cs
@@ -0,0 +1,14 @@
+﻿using BL.DTOs;
+using Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Contracts.Shipment
+{
+    public interface IShipment : IBaseService<TbShipment, ShipmentDTO>
+    {
+    }
+}
