bddf673630f815ab63d32a2fb76f62e6d5e33378
Haidy Gabal <haidy.a.gabal2024@gmail.com>
2026-04-10 15:13:22 +0200

    update this services

---
+++ BL/Services/UserSenderService.cs
@@ -0,0 +1,33 @@
+﻿using AutoMapper;
+using BL.Contracts;
+using BL.DTOs;
+
+using Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Services
+{
+    public class UserSenderService : BaseService<TbUserSender, UserSenderDTO>, IUserSender
+    {
+
+        private readonly IMapper _mapper;
+        IUnitOfWork _uow;
+
+
+        public UserSenderService(IUnitOfWork uow, IMapper mapper) :
+            base(uow, mapper)
+        {
+
+         
+            _uow = uow;
+            _mapper = mapper;
+        }
+
+        
+    }
+    
+}
