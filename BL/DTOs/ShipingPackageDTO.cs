38fab9283b4ea40433ea7d3b3242c5f459ac3a77
Haidy Gabal <haidy.a.gabal2024@gmail.com>
2026-04-10 15:07:58 +0200

    add shipingPackages

---
+++ BL/DTOs/ShipingPackageDTO.cs
@@ -0,0 +1,23 @@
+﻿using BL.DTOs.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.DTOs
+{
+    public class ShipingPackageDTO : BaseDTOs
+    {
+
+        [Required(ErrorMessage = "Arabic name is required")]
+        [StringLength(200, ErrorMessage = "Arabic name cannot exceed 200 characters")]
+        public string? TbShipingPackagesAname { get; set; }
+
+        [Required(ErrorMessage = "English name is required")]
+        [StringLength(200, ErrorMessage = "English name cannot exceed 200 characters")]
+        public string? TbShipingPackagesEname { get; set; }
+
+    }
+}
---
+++ BL/Services/ShipingPackageService.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BL.Contracts;
+using BL.DTOs;
+using Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Services
+{
+    public class ShipingPackageService
+     : BaseService<TbShipingPackage, ShipingPackageDTO>, IShipingPackages
+    {
+        public ShipingPackageService(IRepository<TbShipingPackage> repo, IMapper mapper)
+            : base(repo, mapper)
+        {
+        }
+
+     
+    }
+
+}
