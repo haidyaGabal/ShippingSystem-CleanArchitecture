102841be8a14c1aa5e8bf0876c83ff2c5bde81d9
Haidy Gabal <haidy.a.gabal2024@gmail.com>
2026-04-10 15:12:04 +0200

    update this services

---
+++ BL/Services/shipingTypeServices.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using BL.Contracts;
+using BL.DTOs;
+using DAL;
+using DAL.Repositories;
+using Domains;
+
+namespace BL.Services
+{
+
+
+    public class ShipingTypeService : BaseService<TbShipingType, ShipingTypeDTO>, IShipingType
+    {
+        public ShipingTypeService(IRepository<TbShipingType> repo, IMapper mapper) : base(repo, mapper)
+        {
+
+        }
+    }
+}
