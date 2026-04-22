d1e6474d1ae5c6516a82ee2903f5eaecc7fe9863
Haidy Gabal <haidy.a.gabal2024@gmail.com>
2026-04-22 23:20:35 +0200

    add RefreshTokenRetriver to handle with user null

---
+++ BL/Contracts/IRefreshTokenRetriver.cs
@@ -0,0 +1,15 @@
+﻿using BL.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Contracts
+{
+    public interface IRefreshTokenRetriver
+    {
+
+        public Task<RefreshTokenDTO> GetByToken(string token);
+    }
+}
---
+++ BL/Services/RefreshTokenRetriverService.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using BL.Contracts;
+using BL.DTOs;
+using Domains;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.Services
+{
+    public class RefreshTokenRetriverService : IRefreshTokenRetriver
+    {
+        IRepository<TbRefreshToken> _repo;
+        IMapper _mapper;
+        public RefreshTokenRetriverService(IRepository<TbRefreshToken> repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+        public async Task<RefreshTokenDTO> GetByToken(string token)
+        {
+            var refreshToken =await _repo.FirstOrDefault(a => a.Token == token);
+            return _mapper.Map<TbRefreshToken, RefreshTokenDTO>(refreshToken);
+        }
+
+    
+    }
+}
