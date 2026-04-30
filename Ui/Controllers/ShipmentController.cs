a2b8e22226fa3ce21dfa5d22e01185bc2856de73
Haidy Gabal <haidy.a.gabal2024@gmail.com>
2026-04-30 23:22:19 +0300

    add ShipmentController

---
+++ Ui/Controllers/ShipmentController.cs
@@ -0,0 +1,31 @@
+﻿
+using BL.Contracts.Shipment;
+using Domains;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ui.Controllers
+{
+    public class ShipmentController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IShipment _shipment;
+        public ShipmentController(ILogger<HomeController> logger, IShipment shipment)
+        {
+            _logger = logger;
+            _shipment = shipment;
+            
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+         
+            return View();
+        }
+
+        
+    }
+} 
