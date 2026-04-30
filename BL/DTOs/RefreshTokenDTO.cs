c6f6d490a8731316cbb46b54a639baf1643ea296
Haidy Gabal <haidy.a.gabal2024@gmail.com>
2026-04-30 23:21:08 +0300

    RefreshTokenDTO

---
+++ BL/DTOs/RefreshTokenDTO.cs
@@ -0,0 +1,22 @@
+﻿using BL.DTOs.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BL.DTOs
+{
+    public class RefreshTokenDTO:BaseDTOs
+    {
+        public string Token { get; set; }
+
+        public string UserId { get; set; }
+
+        public DateTime Expires { get; set; }
+
+        public int CurrentState { get; set; }
+    }
+}
